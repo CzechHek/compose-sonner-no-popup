2f0bcbfa5c9f8a59fa66f84a7cff17479a32d61f
durinmic <durinmic@fel.cvut.cz>
2026-06-18 01:45:51 +0200

    Simplify-root-build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.compose.compiler) apply false
+    id("org.jetbrains.kotlin.jvm") version "2.3.10" apply false
+    id("org.jetbrains.compose") version "1.10.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
 }
